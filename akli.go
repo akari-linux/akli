5315b6dbede7f4bd4d06aaaf1b7bcfd7e3d92cab
a7mz1 <295322090+a7mz1@users.noreply.github.com>
2026-08-16 20:34:55 +0200

    feat: add version check

--- akli.go
+++ akli.go
@@ -4,12 +4,17 @@ import (
 	"github.com/alecthomas/kong"
 )
 
+const AkliVersion string = "0.0.1pa"
+
 var CLI struct {
-	Verbose bool `short:"v" help:"Enable verbosity."`
+	Verbose bool             `short:"v" help:"Enable verbosity."`
+	Version kong.VersionFlag `short:"V" help:"Print version and exit."`
 }
 
 func main() {
-	ctx := kong.Parse(&CLI)
+	ctx := kong.Parse(&CLI, kong.Vars{
+		"version": AkliVersion,
+	})
 	switch ctx.Command() {
 	default:
 		ctx.PrintUsage(false)
