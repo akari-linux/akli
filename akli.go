0d372ae2e7061c8cf8001ffcf6aad38314063cc9
a7mz1 <295322090+a7mz1@users.noreply.github.com>
2026-08-17 14:15:44 +0200

    feat: add initial logging and GPU driver commands

--- akli.go
+++ akli.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"os"
+	"runtime"
+
+	"charm.land/log/v2"
 	"github.com/alecthomas/kong"
 )
 
-const AkliVersion string = "0.0.1pa"
+const AkliVersion string = "0.0.2pa"
 
 var CLI struct {
 	Verbose bool             `short:"v" help:"Enable verbosity."`
 	Version kong.VersionFlag `short:"V" help:"Print version and exit."`
+
+	Driver struct {
+		GPU struct {
+			Install struct {
+			} `cmd:"" help:"Automatically installs any required graphics drivers"`
+
+			Remove struct {
+			} `cmd:"" help:"Automatically removes installed graphics drivers"`
+		} `cmd:"" help:"Manage GPU drivers."`
+	} `cmd:"" help:"Manage your system's drivers."`
 }
 
 func main() {
+	var logger = log.New(os.Stderr)
+	logger.SetLevel(log.DebugLevel)
+
 	ctx := kong.Parse(&CLI, kong.Vars{
 		"version": AkliVersion,
 	})
+
+	if CLI.Verbose {
+		logger.Debugf("Running akli on %s with %s", runtime.GOOS, runtime.Version())
+	}
+
 	switch ctx.Command() {
-	default:
-		ctx.PrintUsage(false)
+	case "driver gpu install":
+		// TODO: Implement automatic GPU driver installation
+	case "driver gpu remove":
+		// TODO: Implement automatic GPU driver removal
 	}
 }
